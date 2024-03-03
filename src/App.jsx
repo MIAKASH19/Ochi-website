05a1706c3eda6bf9d582def18ec738baa05dc56c
MIAKASH19 <121384260+MIAKASH19@users.noreply.github.com>
2024-03-04 00:58:46 +0600

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen bg-zinc-900">
       <Navbar />
     </div>
   );
