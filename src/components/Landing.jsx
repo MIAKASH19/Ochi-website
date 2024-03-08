756029c819e037b2fc07e47036dd70f09bf53ba5
MIAKASH19 <121384260+MIAKASH19@users.noreply.github.com>
2024-03-08 22:26:54 +0600

    editing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <div className="text-white bg-zinc-900 w-full h-screen pt-1">
       <div className="textstructure mt-40 px-10">
-        {["we create", "eye opening", "presentations"].map(
+        {["we create", "eye-opening", "presentations"].map(
           (masktext, index) => {
             return (
               <div key={index} className="masker ">
@@ -15,6 +15,14 @@ const Landing = () => {
             );
           }
         )}
+      </div>
+      <div className="border-t-[1px] border-zinc-600 mt-16 flex justify-between items-center p-3 px-10">
+        {[
+          "For public and provate companies",
+          "From the first pitch to IPO",
+        ].map((item, index) => {
+            return <p className="text-md font-neue leading-none">{item}</p>
+        })}
       </div>
     </div>
   );
