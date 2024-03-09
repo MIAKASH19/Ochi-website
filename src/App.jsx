90a18f233f134334487b8948bdc52680aabb0d1a
MIAKASH19 <121384260+MIAKASH19@users.noreply.github.com>
2024-03-10 00:00:37 +0600

    editing firse

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import Landing from "./components/Landing";
+import Marque from "./components/Marque";
 
 function App() {
   return (
-    <div className="w-full h-screen  ">
+    <div className="w-full min-h-screen bg-zinc-900 ">
       <Navbar />
       <Landing />
+      <Marque />
     </div>
   );
 }
---
+++ src/components/Marque.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Marque() {
+  return (
+    <div className="w-full py-10 bg-[#004D43] overflow-hidden rounded-tl-3xl rounded-tr-3xl">
+      <div className="text flex gap-16 border-t-[1px] border-zinc-400 border-b-[1px] whitespace-nowrap">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[25vw] -mb-4 -mt-12 text-white font-founder uppercase leading-none"
+        >
+          We are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[25vw] -mb-4 -mt-12 text-white font-founder uppercase leading-none"
+        >
+          We are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[25vw] -mb-4 -mt-12 text-white font-founder uppercase leading-none"
+        >
+          We are ochi
+        </motion.h1>
+      </div>
+    </div>
+  );
+}
